a84102bc543615048cd25e827ebb8ffdef43c917
wmontes9 <43587256+wmontes9@users.noreply.github.com>
2021-01-12 17:53:45 -0500

    prueba template

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1749,10 +1749,10 @@
      <script src="{{ asset('template/admin/files/bower_components/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
      <script src="{{ asset('template/admin/files/assets/pages/data-table/js/jszip.min.js')}}"></script>
      <script src="{{ asset('template/admin/files/assets/pages/data-table/js/pdfmake.min.js')}}"></script>
-     <script src="{{ asset('template/admin/files/assets/pages/data-table/js/vfs_fonts.js')}}"></script>
      <script src="{{ asset('template/admin/files/bower_components/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
      <script src="{{ asset('template/admin/files/bower_components/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
      <script src="{{ asset('template/admin/files/assets/pages/data-table/js/dataTables.bootstrap4.min.js')}}"></script>
+     <script src="{{ asset('template/adm in/files/assets/pages/data-table/js/vfs_fonts.js')}}"></script>
      <script src="{{ asset('template/admin/files/bower_components/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
      <script src="{{ asset('template/admin/files/bower_components/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
  
